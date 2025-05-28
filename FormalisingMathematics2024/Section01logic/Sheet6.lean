8087ea39a2301f5d906ef2c144ede1e3c129f4b1
YanYablonovskiy <yablonovskiy.yan@gmail.com>
2025-05-28 17:22:10 +1000

    Update Sheet6.lean

--- FormalisingMathematics2024/Section01logic/Sheet6.lean
+++ FormalisingMathematics2024/Section01logic/Sheet6.lean
@@ -129,22 +129,87 @@ example : (P ∨ Q) ∨ R ↔ P ∨ Q ∨ R :=
 
 
 example : (P → R) → (Q → S) → P ∨ Q → R ∨ S := by
-  sorry
+  intro hpr hqs hpOq
+  cases' hpOq with hp hq
+  · left; exact hpr hp
+  · right; exact hqs hq 
+  done
+
+
+example : (P → R) → (Q → S) → P ∨ Q → R ∨ S := by
+  rintro hpr hqs (hp|hq)
+  · exact Or.inl (hpr hp)
+  · exact Or.inr (hqs hq)
+
+
+example : (P → R) → (Q → S) → P ∨ Q → R ∨ S := 
+  fun hpr hqs hpOq ↦ 
+   hpOq.elim (fun hp ↦ Or.inl (hpr hp)) ((fun hs ↦ Or.inr (hqs hs)))
+
+example : (P → R) → (Q → S) → P ∨ Q → R ∨ S 
+| hpr, hqs, hpOq => hpOq.elim (fun hp ↦ Or.inl (hpr hp)) ((fun hs ↦ Or.inr (hqs hs)))
+
+example : (P → Q) → P ∨ R → Q ∨ R := by
+  intro hpq hpOr 
+  cases' hpOr with hp hr
+  · left; apply hpq hp
+  · right; exact hr
   done
 
 example : (P → Q) → P ∨ R → Q ∨ R := by
-  sorry
+  rintro hpq (hp|hr)
+  · left; apply hpq hp
+  · right; exact hr
   done
 
+
+example : (P → Q) → P ∨ R → Q ∨ R := 
+ fun hpq hpOr ↦ hpOr.elim (fun hp ↦ Or.inl (hpq hp)) (fun hr ↦ Or.inr hr)
+
+example : (P → Q) → P ∨ R → Q ∨ R
+| hpq, hpOr => hpOr.elim (fun hp ↦ Or.inl (hpq hp)) (fun hr ↦ Or.inr hr)
+
 example : (P ↔ R) → (Q ↔ S) → (P ∨ Q ↔ R ∨ S) := by
-  sorry
-  done
+  intro hpIr hqIs 
+  constructor <;> (rw [hpIr,hqIs]; intro h; exact h)
+
+#check Eq.subst
+
+example : (P ↔ R) → (Q ↔ S) → (P ∨ Q ↔ R ∨ S) := fun hpIr hqIs ↦ 
+ have h1:_ := propext hpIr 
+ have h2:_ := propext hqIs  
+ have h3: (P ∨ Q) = (R ∨ Q) := 
+  have : (P ∨ Q) = (P ∨ Q) := Eq.refl (P ∨ Q)
+  Eq.subst h1 this (motive := fun a ↦ (P ∨ Q) = (a ∨ Q)) 
+ have h4: (R ∨ Q) = (R ∨ S) :=
+  have : (R ∨ Q) = (R ∨ Q) := Eq.refl (R ∨ Q)
+  Eq.subst h2 this (motive := fun a ↦ (R ∨ Q) = (R ∨ a)) 
+ have := Eq.trans h3 h4
+ Eq.subst this (Iff.refl (P ∨ Q)) 
+
 
 -- de Morgan's laws
 example : ¬(P ∨ Q) ↔ ¬P ∧ ¬Q := by
-  sorry
+  constructor
+  · intro hnpq
+    constructor
+    · intro hp
+      exact hnpq (Or.inl hp)
+    · intro hq
+      exact hnpq (Or.inr hq)
+  · intro ⟨np,nq⟩
+    rintro (hp|hq)
+    · exact (np hp)
+    · exact (nq hq)
   done
 
+example : ¬(P ∨ Q) ↔ ¬P ∧ ¬Q := 
+ ⟨fun hnpq ↦ ⟨fun hp ↦ hnpq (Or.inl hp) , fun hq ↦ hnpq (Or.inr hq) ⟩, 
+  fun ⟨hnp,hnq⟩ ↦ (fun hpOq ↦ hpOq.elim (fun hp ↦ hnp hp) (fun hq ↦ hnq hq)) ⟩
+
 example : ¬(P ∧ Q) ↔ ¬P ∨ ¬Q := by
-  sorry
+  constructor
+  · intro hnpOq 
+    sorry
+  · sorry
   done
