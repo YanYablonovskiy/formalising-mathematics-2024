708cb5d387d3ad407657da4e97c96a84afbcae29
YanYablonovskiy <yablonovskiy.yan@gmail.com>
2025-05-28 22:37:39 +1000

    Update Sheet2.lean

--- FormalisingMathematics2024/Section02reals/Sheet2.lean
+++ FormalisingMathematics2024/Section02reals/Sheet2.lean
@@ -21,12 +21,12 @@ knows that the real numbers are a ring. See if you can use
 
 -/
 variable (x y: ℝ)
-#check mul_add (x*y) 
+#check mul_add (x*y)
 #check mul_one
 #check add_assoc (x^2)
 #check one_add_one_eq_two
 #check add_pow_two
-
+#check add_pow
 example (x y : ℝ) : (x + y) ^ 2 = x ^ 2 + 2 * x * y + y ^ 2 := by
   calc (x+y)^2 = (x+y)*(x+y)            := by rw [pow_two]
        _       =  (x+y)*x + (x+y)*y     := by rw [mul_add]
@@ -48,17 +48,34 @@ example (x y : ℝ) : (x + y) ^ 2 = x ^ 2 + 2 * x * y + y ^ 2 := add_pow_two x y
 
 example : ∀ a b : ℝ, ∃ x, (a + b) ^ 3 = a ^ 3 + x * a ^ 2 * b + 3 * a * b ^ 2 + b ^ 3 := by
   intro a b
-  sorry
+  use 3
+  rw [add_pow]
+  simp [Finset.sum]
+  ring
   done
 
+
+example : ∀ a b : ℝ, ∃ x, (a + b) ^ 3 = a ^ 3 + x * a ^ 2 * b + 3 * a * b ^ 2 + b ^ 3 := by
+  intro a b
+  use 3
+  ring
+
+
 example : ∃ x : ℝ, ∀ y, y + y = x * y := by
-  sorry
+  use 2
+  intro y
+  ring
   done
 
 example : ∀ x : ℝ, ∃ y, x + y = 2 := by
-  sorry
+  intro x
+  use (2-x)
+  ring
   done
 
 example : ∀ x : ℝ, ∃ y, x + y ≠ 2 := by
-  sorry
+  intro x
+  use 3-x
+  ring
+  linarith
   done
