3e49e8bf3dd9064d30a80750779229bda04dd8e8
YanYablonovskiy <yablonovskiy.yan@gmail.com>
2025-06-10 21:08:03 +1000

    Update Sheet6.lean

--- FormalisingMathematics2024/Section04sets/Sheet6.lean
+++ FormalisingMathematics2024/Section04sets/Sheet6.lean
@@ -36,27 +36,122 @@ for group theory. In Lean we use the notation `f ⁻¹' T` for this pullback.
 
 -/
 
+#check Function.invFunOn_image_image_subset
+
 variable (X Y : Type) (f : X → Y) (S : Set X) (T : Set Y)
 
-example : S ⊆ f ⁻¹' (f '' S) := by sorry
+example : S ⊆ f ⁻¹' (f '' S) := by
+ intro x xS
+ simp
+ use x
 
-example : f '' (f ⁻¹' T) ⊆ T := by sorry
+
+example : S ⊆ f ⁻¹' (f '' S) := by
+ intro x xS
+ rw [Set.image,Set.preimage]
+ use x
+
+
+
+example : f '' (f ⁻¹' T) ⊆ T := by exact Set.image_preimage_subset f T
+
+
+example : f '' (f ⁻¹' T) ⊆ T := by
+ intro x hx
+ simp at hx
+ obtain ⟨x1,hx1⟩ := hx
+ rw [hx1.2] at hx1
+ exact hx1.1
 
 -- `library_search` will do this but see if you can do it yourself.
-example : f '' S ⊆ T ↔ S ⊆ f ⁻¹' T := by sorry
-
+example : f '' S ⊆ T ↔ S ⊆ f ⁻¹' T := by
+ constructor
+ · intro h
+   rw [Set.preimage]
+   intro x xs
+   rw [Set.image] at h
+   rw [Set.mem_def,setOf]
+   have: f x ∈ {x | ∃ a ∈ S, f a = x} := by
+    use x
+   exact h this
+ · intro hsf y yfs
+   rw [Set.image] at yfs
+   obtain ⟨y1,fxy1⟩ := yfs
+   specialize hsf fxy1.1
+   rw [Set.preimage] at hsf
+   rw [Set.mem_def,setOf] at hsf
+   rw [fxy1.2] at hsf
+   exact hsf
 -- Pushforward and pullback along the identity map don't change anything
 -- pullback is not so hard
-example : id ⁻¹' S = S := by sorry
+example : id ⁻¹' S = S := by
+ rfl
 
 -- pushforward is a little trickier. You might have to `ext x, split`.
-example : id '' S = S := by sorry
+example : id '' S = S := by
+ ext x
+ constructor
+ · intro hxf
+   rw [Set.image] at hxf
+   obtain ⟨x1,hx1⟩ := hxf
+   rw [id] at hx1
+   rw [hx1.2] at hx1
+   exact hx1.1
+ · intro hxS
+   rw [Set.image]
+   use x
+   rw [id]
+   constructor
+   · exact hxS
+   · rfl
 
 -- Now let's try composition.
 variable (Z : Type) (g : Y → Z) (U : Set Z)
 
 -- preimage of preimage is preimage of comp
-example : g ∘ f ⁻¹' U = f ⁻¹' (g ⁻¹' U) := by sorry
+example : g ∘ f ⁻¹' U = f ⁻¹' (g ⁻¹' U) := by
+ ext x
+ constructor
+ · intro hx
+   rw [Set.preimage,setOf] at *
+   rw [Set.preimage]
+   rw [Function.comp] at hx
+   rw [setOf]
+   rw [Set.mem_def] at *
+   rw [Set.mem_def] at hx
+   rw [Set.mem_def]
+   assumption
+ · intro hx
+   simp only [Set.preimage,setOf,Function.comp] at *
+   assumption
+
+
+
+
+
 
 -- preimage of preimage is preimage of comp
-example : g ∘ f '' S = g '' (f '' S) := by sorry
+example : g ∘ f '' S = g '' (f '' S) := by
+ ext x
+ constructor
+ · intro hx
+   rw [Set.image]
+   simp only [Set.image,Function.comp] at hx
+   obtain ⟨x1,hx1⟩ := hx
+   use (f x1)
+   rw [Set.image]
+   constructor
+   · use x1
+     exact ⟨hx1.1,rfl⟩
+   · exact hx1.2
+ · intro hx
+   simp only [Set.image,Function.comp]
+   rw [Set.image] at hx
+   obtain ⟨x1,hx1⟩ := hx
+   simp only [Set.image] at hx1
+   obtain ⟨a,ha⟩ := hx1.1
+   use a
+   constructor
+   · exact ha.1
+   · rw [ha.2]
+     exact hx1.2
